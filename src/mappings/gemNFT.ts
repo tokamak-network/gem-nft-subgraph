87c8168b97677c3e9f669e10025e7baac7d3e75b
jusdy <pauleta0126@gmail.com>
2024-10-15 11:11:19 +0900

    update address & cooldown entity

--- src/mappings/gemNFT.ts
+++ src/mappings/gemNFT.ts
@@ -21,50 +21,6 @@ import {
   TradeHistory
 } from '../../generated/schema';
 
-import { TradeType } from '../utils';
-
-// export function handleTransfer(event: Transfer): void {
-//   let nft = NFT.load(event.params.tokenId.toString());
-//   if (!nft) {
-//     nft = new NFT(event.params.tokenId.toString());
-//     nft.ownerHistory = [];
-//     nft.timeHistory = [];
-//   }
-//   nft.owner = event.params.to;
-//   nft.tokenID = event.params.tokenId;
-//   nft.transferTime = event.block.timestamp;
-//   let ownerHistory = nft.ownerHistory;
-//   ownerHistory!.push(event.params.to);
-//   nft.ownerHistory = ownerHistory;
-//   let timeHistory = nft.timeHistory;
-//   timeHistory!.push(event.block.timestamp);
-//   nft.timeHistory = timeHistory;
-//   nft.save();
-// }
-
-// export function handleApproval(event: ApprovalEvent): void {
-//   let approval = Approval.load(event.params.owner.toHexString() + event.params.approved.toHexString());
-//   if (!approval) {
-//     approval = new Approval(event.params.owner.toHexString() + event.params.approved.toHexString());
-//   }
-//   approval.from = event.params.owner;
-//   approval.to = event.params.approved;
-//   approval.tokenID = event.params.tokenId;
-//   approval.forAll = false;
-//   approval.save();
-// }
-// export function handleApprovalforAll(event: ApprovalForAll): void {
-//   let approval = Approval.load(event.params.owner.toHexString() + event.params.operator.toHexString());
-//   if (!approval) {
-//     approval = new Approval(event.params.owner.toHexString() + event.params.operator.toHexString());
-//   }
-//   approval.from = event.params.owner;
-//   approval.to = event.params.operator;
-//   approval.tokenID = BigInt.fromI32(-1);
-//   approval.forAll = true;
-//   approval.save();
-// }
-
 export function handleCreated(event: Created): void {
   let nft = new NFT(event.params.tokenId.toString());
   nft.tokenID = event.params.tokenId;
@@ -72,7 +28,7 @@ export function handleCreated(event: Created): void {
   nft.color = event.params.color;
   nft.quadrants = event.params.quadrants;
   nft.value = event.params.value;
-  nft.gemCooldownPeriod = event.params.cooldownPeriod;
+  nft.cooldownDueDate = event.params.cooldownDueDate;
   nft.miningPeriod = event.params.miningPeriod;
   nft.owner = event.params.owner;
   nft.isMining = false;
